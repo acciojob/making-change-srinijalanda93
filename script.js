1ba940a228048d4627d13ba80262b5f86bbd941c
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-19 12:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const makeChange = (c) => {
 	let units={q:25,d:10,n:5,p:1}
 let output={};
 	for(let i in units){
-		let currentunit=units[i]; //1
-		output[i]=c%currentunit;
+	  	let currentunit=units[i]; //1
+		//output[i]=c%currentunit;
+		output[i]= Math.floor(c / currentunit);
 		c=c-currentunit*output[i];
-	}	
+	}
+	return output; 
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+//const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
